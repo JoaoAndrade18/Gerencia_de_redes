b788f1b957614812b9d4596613199041fb7d08c8
João Batista Andrade <111291896+JoaoAndrade18@users.noreply.github.com>
2023-07-01 20:52:43 -0300

    Complete add
    versão concluida

--- GerenciaDeRede.sh
+++ GerenciaDeRede.sh
@@ -4,8 +4,7 @@ L_SIMBOLOS=("|" "/" "-" "\\")
 INTERVALO=0.2
 T_SIMBOLOS=${#L_SIMBOLOS[@]}
 
-
-echo 
+echo
 echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 echo "		    Gerência da Rede 		               "
 echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -40,33 +39,33 @@ while true; do
 	  		ip -c a
 		;;
 		2)
-			echo "Configurando IP e Mascara de forma TEMPORARIA!!"
-			echo "Informe o nome da interface de rede: "
+			echo " - Configurando IP e Mascara de forma TEMPORÁRIA!!"
+			echo " - Informe o nome da interface de rede: "
 			read INTERFACE_NAME
-			echo "Informe o endereço ip desejado: "
+			echo " - Informe o endereço ip desejado: "
 			read ENDERECO_IP
-			echo "Informe o endereço da mascara de rede: "
+			echo " - Informe o endereço da mascara de rede: "
 			read MASCARA
 			sudo ifconfig $INTERFACE_NAME $ENDERECO_IP netmask $MASCARA
 		;;
 		3)
-			echo "Informe o nome da interface de rede a ser habilitada:"
+			echo " - Informe o nome da interface de rede a ser habilitada:"
 			read H_REDE
 			sudo ifconfig $H_REDE up
 		;;
 		4)
-			echo "Informe o nome da interface de rede a ser desabilitada:"
+			echo " - Informe o nome da interface de rede a ser desabilitada:"
 			read D_REDE
 			sudo ifconfig $D_REDE down
 		;;
 	 	5)
-			echo "Informe seu SO: 1 - Debian, 2 - Ubuntu"
+			echo " - Informe seu SO: 1 - Debian, 2 - Ubuntu"
 			read DISTRO
 			if [ $DISTRO = "1" ]; then
-				echo "Você quer editar na mão ou deseja só informar as credenciais? 1 - manual, 2 - facilitado"
+				echo " - Você quer editar na mão ou deseja só informar as credenciais? 1 - manual, 2 - automatizado"
 				read METODO
 				if [ $METODO = "1" ]; then
-					echo "Escolha um editor: 1 - vi, 2 - nano"
+					echo " - Escolha um editor: 1 - vi, 2 - nano"
 					read EDITOR
 					if [ $EDITOR = "1" ]; then
 						sudo vi /etc/network/interfaces
@@ -76,40 +75,53 @@ while true; do
 						echo "(｡•́︿•̀｡) Editor invalido!"
 					fi
 				elif [ $METODO = "2" ]; then
-					echo "Informe o nome da interface: "
+					echo " - Informe o nome da interface: "
 					read V_INTERFACE
-					echo "Informe o endereço IP desejado: "
+					echo " - Informe o endereço IP desejado: "
 					read V_IP
-					echo "Informe a mascara de rede desejada: "
+					echo " - Informe a mascara de rede desejada: "
 					read V_MASCARA
 					FILE="/etc/network/interfaces"
-					sudo bash -c "echo \"#interface de rede $V_INTERFACE
+					sudo bash -c "echo "#interface de rede $V_INTERFACE
 auto $V_INTERFACE
 allow-hotplug $V_INTERFACE
 iface $V_INTERFACE inet static
 	address $V_IP
-	netmask $V_MASCARA\" >> \"$FILE\""
+	netmask $V_MASCARA" >> \"$FILE\""
 					echo "operação realizada com sucesso!"
+					echo "Realize a reinicialização da rede para que as alterações tenham efeito!" 
+					echo Use o comando a seguir caso esteja no debian: sudo systemctl restart networking.service
+					echo Caso nao tenha funcionado, reveja o arquivo /etc/network/interfaces e verifique se as informações estão corretas.
 				else
 					echo "(｡•́︿•̀｡) Opcao invalida!"
 				fi
 			elif [ $DISTRO = "2" ]; then
-				echo
+				echo "Não implementado ainda!"
 			else
 				echo "(｡•́︿•̀｡) SO invalido!"
 			fi
 		;;
 		6)
-		
+			sudo dhclient
 		;;
 		7)
 			route
 		;;
 		8)
-		
+			FILE="/etc/network/interfaces"
+			echo " - Informe a linha onde será inserido o geteway, lembrando que as linhas posteriores serão deslocadas para baixo:"
+			read LINHA
+			echo " - Informe o gateway desejado: "
+			read GATEWAY0
+			GATEWAY="        gateway $GATEWAY0"
+			sudo sed -i "${LINHA}i\\""$GATEWAY" "$FILE"
 		;;
 		9)
-		
+			FILE="/etc/network/interfaces"
+			echo " - Informe a linha onde será deletado o gateway: lembrando que essa operação irá apagar a linha informada mesmo que não seja o gateway:\
+ o restante será deslocado para cima!"
+			read LINHA2
+			sudo sed -i "${LINHA2}d" "$FILE"
 		;;
 		0)
 			exit
@@ -127,12 +139,12 @@ iface $V_INTERFACE inet static
 		echo 6 – Obter IP via DHCP
 		echo 7 – Tabela de Rotas
 		echo 8 – Adicionar Gateway
-		echo 9 – Deletar gateway
+		echo 9 – Deletar Gateway
 		echo 0 – Sair
 		echo
 		;;
 		*)
-			echo "(｡•́︿•̀｡) Número inválido."
+			echo "(｡•́︿•̀｡) Opção inválida."
 		;;
 	esac
 done
